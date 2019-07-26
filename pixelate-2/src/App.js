f254715781d893f9c60a90358453f562a54a07f5
Steven Li <ilumii@users.noreply.github.com>
2019-07-26 15:18:36 -0400

    updated

--- pixelate-2/src/App.js
+++ pixelate-2/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
-import Pixelate from './Components/Pixelate';
+import Pixelate from './Components/Pixelate.js';
+import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      column: 0,
+    }
+  }
+
+  addColumn = () =>{
+    let temp = this.state.column +1
+    this.setState({
+      column: temp
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <Pixelate></Pixelate>
+      <div className="center">
+        <button onClick={()=>this.addColumn()}>Try me</button>
+        
       </div>
     );
   }
